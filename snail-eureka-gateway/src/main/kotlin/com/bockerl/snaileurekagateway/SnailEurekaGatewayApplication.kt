a1328974aaf07f6f19881f4c2675ca8ffac6dabe
Vorschlag-bit <bustout@naver.com>
2025-01-27 19:03:24 +0900

    ✨ feat: 프로젝트 msa 폴더 구조 세팅

---
+++ snail-eureka-gateway/src/main/kotlin/com/bockerl/snaileurekagateway/SnailEurekaGatewayApplication.kt
@@ -0,0 +1,11 @@
+package com.bockerl.snaileurekagateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnailEurekaGatewayApplication
+
+fun main(args: Array<String>) {
+    runApplication<SnailEurekaGatewayApplication>(*args)
+}

1cc600affdfcea98658d08646771680d16e3b522
woosuk1 <giuseog@gmail.com>
2025-02-04 03:18:12 +0900

    ✨feat: 게시글 상세 조회 mapper 구현, 전 계층 구현 완료

---
+++ snail-member/src/main/kotlin/com/bockerl/snailmember/board/query/repository/BoardMapper.kt
@@ -0,0 +1,11 @@
+package com.bockerl.snailmember.board.query.repository
+
+import com.bockerl.snailmember.board.command.application.dto.BoardDTO
+import com.bockerl.snailmember.board.command.domain.aggregate.entity.Board
+import org.apache.ibatis.annotations.Mapper
+
+@Mapper
+interface BoardMapper {
+    /* 설명. boardDTO에는 long값 받는건 어떤지? */
+    fun selectBoardByBoardId(boardId: Long): Board?
+}

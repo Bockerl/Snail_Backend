d75f75e280611edfc6ba5659538eb314b2726c89
Vorschlag-bit <bustout@naver.com>
2025-02-05 14:34:58 +0900

    ✨ feat: 지역 db 저장 로직 구현

---
+++ snail-member/src/main/kotlin/com/bockerl/snailmember/area/command/domain/aggregate/entity/SiggAreas.kt
@@ -0,0 +1,36 @@
+package com.bockerl.snailmember.area.command.domain.aggregate.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "sigg_areas")
+class SiggAreas(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sigg_areas_id")
+    var siggAreasId: Long? = null,
+    @Column(name = "sido_areas_id")
+    var sidoAreasId: Long,
+    @Column(name = "sigg_areas_adm_code")
+    var siggAreasAdmCode: String,
+    @Column(name = "sigg_areas_name")
+    var siggAreasName: String,
+) {
+    companion object {
+        fun create(
+            sidoAreasId: Long,
+            admCode: String,
+            name: String,
+        ) = SiggAreas(
+            siggAreasId = 0,
+            sidoAreasId = sidoAreasId,
+            siggAreasAdmCode = admCode,
+            siggAreasName = name,
+        )
+    }
+}

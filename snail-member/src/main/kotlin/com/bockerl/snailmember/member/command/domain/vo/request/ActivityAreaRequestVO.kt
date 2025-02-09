6aef8ff8a4b37c49f2d2f73b020935d69c35616c
Vorschlag-bit <bustout@naver.com>
2025-02-09 23:08:03 +0900

    🐛 fix: TestConfiguration 통일로 초기화 횟수 1회로 통일

--- snail-member/src/main/kotlin/com/bockerl/snailmember/member/command/domain/vo/request/ActivityAreaRequestVO.kt
+++ snail-member/src/main/kotlin/com/bockerl/snailmember/member/command/domain/vo/request/ActivityAreaRequestVO.kt
@@ -1,8 +1,7 @@
 package com.bockerl.snailmember.member.command.domain.vo.request
 
-import com.fasterxml.jackson.annotation.JsonProperty
-
 data class ActivityAreaRequestVO(
-    @field:
-    @JsonProperty("activity_area")
+//    @field:
+//    @JsonProperty("activity_area")
+    val title: String,
 )
--- snail-member/src/test/kotlin/com/bockerl/snailmember/config/TestConfiguration.kt
+++ snail-member/src/test/kotlin/com/bockerl/snailmember/config/TestConfiguration.kt
@@ -6,9 +6,21 @@ import org.mockito.Mockito.mock
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Primary
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.mail.javamail.JavaMailSender
 
 @TestConfiguration
-class RegistrationTestConfiguration {
+class TestConfiguration {
+    // SpringBootTest는 실제 Application처럼 동작하기 위해 전체 Application Context를 로드합니다.
+    // 이 과정에서 컴포넌트 스캔으로 실제 서비스에 사용될 bean과 테스트에 사용될 bean의 충돌이 발생해서 @Primary를 달았습니다.
+    @Primary
+    @Bean
+    fun mockRedisTemplate(): RedisTemplate<String, String> = mock()
+
+    @Primary
+    @Bean
+    fun mockMailSender() = mock(JavaMailSender::class.java)!!
+
     @Primary
     @Bean
     fun mockAuthService() = mock(AuthService::class.java)!!

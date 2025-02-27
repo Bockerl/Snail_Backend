c41134a7762315392ec81c98ed4687cf6b90891c
Vorschlag-bit <bustout@naver.com>
2025-02-27 23:49:18 +0900

    ✨ feat: AuthenticationEntryPoint 구현

---
+++ snail-member/src/main/kotlin/com/bockerl/snailmember/security/CustomAuthenticationEntryPoint.kt
@@ -0,0 +1,44 @@
+package com.bockerl.snailmember.security
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.authentication.AccountStatusException
+import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.stereotype.Component
+import java.time.LocalDateTime
+
+@Component
+class CustomAuthenticationEntryPoint : AuthenticationEntryPoint {
+    override fun commence(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        authException: AuthenticationException,
+    ) {
+        val (statusCode, message) = when (authException) {
+            // 블랙리스트
+            is AccountStatusException -> HttpServletResponse.SC_FORBIDDEN to "이 계정은 현재 사용이 제한되어 있습니다."
+            // 잘못된 아이디 비밀번호
+            is BadCredentialsException -> HttpServletResponse.SC_UNAUTHORIZED to "아이디 또는 비밀번호가 일치하지 않습니다."
+            // 인증 실패
+            else -> HttpServletResponse.SC_BAD_REQUEST to "인증에 실패했습니다."
+        }
+
+        // 상태 코드 설정
+        response.status = statusCode
+        response.contentType = "application/json;charset=UTF-8"
+
+        // json 응답 본문 설정
+        val errorResponse = mapOf(
+            "timestamp" to LocalDateTime.now().toString(),
+            "message" to message,
+            "status" to statusCode,
+            "path" to request.requestURL,
+        )
+
+        val objectMapper = ObjectMapper()
+        response.writer.write(objectMapper.writeValueAsString(errorResponse))
+    }
+}

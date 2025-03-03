dee61e934681a6cf287dec4b0632d4336d8f126c
giuseog <giuseog@gmail.com>
2025-03-03 14:22:44 +0900

    ✨feat : 파일 이벤트 파이프라인 구현 중

--- snail-member/src/main/kotlin/com/bockerl/snailmember/file/command/domain/aggregate/event/FileCreatedEvent.kt
+++ snail-member/src/main/kotlin/com/bockerl/snailmember/file/command/domain/aggregate/event/FileCreatedEvent.kt
@@ -2,7 +2,7 @@ package com.bockerl.snailmember.file.command.domain.aggregate.event
 
 import com.bockerl.snailmember.file.command.domain.aggregate.enums.FileTargetType
 
-data class FileEvent(
+data class FileCreatedEvent(
     val fileName: String,
     val fileUrl: String,
     val fileType: String,

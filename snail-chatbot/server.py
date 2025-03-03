c982882822fb4dc228a93078aada485afa450d5f
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2025-03-03 16:47:35 +0900

    ✨ feat: /chat 경로 api 입력 기능 추가

--- snail-chatbot/server.py
+++ snail-chatbot/server.py
@@ -32,6 +32,13 @@ logger = logging.getLogger(__name__)
 
 logger.info("서버가 시작되었습니다!")
 
+
+# 현재 시간 가져오기
+timestamp = datetime.datetime.now().isoformat()  # ISO 형식으로 날짜 및 시간 반환
+
+embeddings = OllamaEmbeddings(model="llama3.1-instruct-8b:latest")
+
+
 # CORS 미들웨어 설정
 app.add_middleware(
     CORSMiddleware,
@@ -51,6 +58,42 @@ async def redirect_root_to_docs():
 async def playground():
     return {"message": "Welcome to the Playground!"}
 
+########### 대화형 인터페이스 ###########
+
+# 입력 데이터 모델
+class InputChat(BaseModel):
+    messages: list[str]
+
+# 대화형 API 엔드포인트
+@app.post("/chat")
+async def chat(input: str = Form(...), file: Optional[UploadFile] = File(None),  document_type: str = "message"):
+    # vector_db는 이제 Chroma 객체로 자동 주입됨
+    try:
+        input_data = InputChat(**json.loads(input))
+        result = {}
+
+        # 대화 ID 생성
+        conversation_id = str(uuid.uuid4())
+
+        # 챗봇 응답 생성
+        result["chatbot_response"] = chat_chain.invoke(input_data.messages)
+
+
+        return JSONResponse(content={"message": "Chat response", "type": document_type, "result": result, "input_messages": input_data.messages})
+
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
+
+# 대화형 채팅 엔드포인트 설정
+add_routes(
+    app,
+    chat_chain.with_types(input_type=InputChat),
+    path="/chat",
+    enable_feedback_endpoint=True,
+    enable_public_trace_link_endpoint=True,
+    playground_type="chat",
+)
 
 # 서버 실행 설정
 if __name__ == "__main__":

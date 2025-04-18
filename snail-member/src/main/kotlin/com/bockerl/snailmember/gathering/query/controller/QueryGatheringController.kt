9dda903a6409344a222d0c74a51655bbd175a309
giuseog <giuseog@gmail.com>
2025-04-18 20:18:36 +0900

    ✨ feat: 모임 조회 api 구현 중

--- snail-member/src/main/kotlin/com/bockerl/snailmember/gathering/query/controller/QueryGatheringController.kt
+++ snail-member/src/main/kotlin/com/bockerl/snailmember/gathering/query/controller/QueryGatheringController.kt
@@ -1,3 +1,75 @@
 package com.bockerl.snailmember.gathering.query.controller
 
-class QueryGatheringController
+import com.bockerl.snailmember.common.ResponseDTO
+import com.bockerl.snailmember.gathering.command.application.mapper.GatheringConverter
+import com.bockerl.snailmember.gathering.query.dto.QueryGatheringResponseDTO
+import com.bockerl.snailmember.gathering.query.service.QueryGatheringService
+import com.bockerl.snailmember.gathering.query.vo.QueryGatheringResponseVO
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/gathering")
+class QueryGatheringController(
+    private val queryGatheringService: QueryGatheringService,
+    private val gatheringConverter: GatheringConverter,
+) {
+    @Operation(
+        summary = "모임 pk로 모임 상세 조회",
+        description = "모임 pk로 모임 정보, 사진첩, 일정, 게시글을 조회합니다.",
+    )
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "모임 pk로 모임 상세 조회 성공",
+                content = [
+                    Content(mediaType = "application/json", schema = Schema(implementation = QueryGatheringResponseVO::class)),
+                ],
+            ),
+        ],
+    )
+    @GetMapping("/detail/{gatheringId}")
+    fun getGatheringByGatheringId(
+        @PathVariable gatheringId: String,
+    ): ResponseDTO<*> {
+        val queryGatheringResponseDTO: QueryGatheringResponseDTO = queryGatheringService.readGatheringByGatheringId(gatheringId)
+
+        // 상세조회는 다른 api 구현 후 하기
+        // 일정, 사진첩, 게시글을 하나에 보여줘야함
+
+//        return ResponseDTO.ok(gatheringConverter.dtoToResponseVO(gatheringDTO))
+        // 변환해야함
+        return ResponseDTO.ok(queryGatheringResponseDTO)
+    }
+
+    @Operation(
+        summary = "모임 전체 조회",
+        description = "모임 전체를 조회합니다.",
+    )
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = " 모임 전체 조회 성공",
+                content = [
+                    Content(mediaType = "application/json", schema = Schema(implementation = QueryGatheringResponseVO::class)),
+                ],
+            ),
+        ],
+    )
+    @GetMapping("")
+    fun getGathering(
+        @RequestParam(required = false) lastId: Long? = null,
+        @RequestParam(defaultValue = "10") pageSize: Int,
+    ): ResponseDTO<List<QueryGatheringResponseVO>> {
+        val gatheringList: List<QueryGatheringResponseVO> = queryGatheringService.readGathering(lastId, pageSize)
+
+//        return ResponseDTO.ok(boardConverter.dtoToResponseVO(boardList))
+        return ResponseDTO.ok(gatheringList)
+    }
+}
--- snail-member/src/main/kotlin/com/bockerl/snailmember/gathering/query/vo/QueryGatheringResponseVO.kt
+++ snail-member/src/main/kotlin/com/bockerl/snailmember/gathering/query/vo/QueryGatheringResponseVO.kt
@@ -1,3 +1,31 @@
 package com.bockerl.snailmember.gathering.query.vo
 
-class QueryGatheringResponseVO
+import com.bockerl.snailmember.gathering.query.enums.QueryGatheringType
+import com.fasterxml.jackson.annotation.JsonProperty
+import io.swagger.v3.oas.annotations.media.Schema
+import java.io.Serializable
+import java.time.LocalDateTime
+
+data class QueryGatheringResponseVO(
+    @field:Schema(description = "모임 고유 번호(PK)", example = "GAT-00000001", type = "String")
+    val gatheringId: String? = null,
+    @field:Schema(description = "모임 정보", example = "달팽이 좋아해요?", type = "String")
+    val gatheringInformation: String? = null,
+    @field:Schema(description = "모임 제목", example = "달팽이", type = "String")
+    val gatheringTitle: String? = null,
+    @field:Schema(description = "모임 타입", example = "HOBBY", type = "String")
+    val queryGatheringType: QueryGatheringType? = null,
+    @field:Schema(description = "모임 지역", example = "신대방", type = "String")
+    val gatheringRegion: String? = null,
+    @field:Schema(description = "게시글 활성화 여부", example = "true", type = "String")
+    val active: Boolean? = null,
+    @field:Schema(
+        description = "회원 사진",
+        example = "https://-4bf8-a32b-7e4bca1e1466.png",
+        type = "String",
+    )
+    @JsonProperty(namespace = "memberPhoto")
+    val memberPhoto: String? = null,
+    @field:Schema(description = "게시글 생성 시각", example = "2024-12-11 14:45:30", type = "LocalDateTime")
+    val createdAt: LocalDateTime? = null,
+) : Serializable

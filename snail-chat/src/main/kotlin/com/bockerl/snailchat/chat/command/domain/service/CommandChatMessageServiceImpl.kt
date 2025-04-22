936f4067b2b844140e98f79e753b1b568f931925
EuiHyeok <dmlgur0517@daum.net>
2025-04-22 22:36:08 +0900

    ✨feat: Redis 원자성 보장을 위한 2차 보증 구현 추가 (#62)

--- snail-chat/src/main/kotlin/com/bockerl/snailchat/chat/command/domain/service/CommandChatMessageServiceImpl.kt
+++ snail-chat/src/main/kotlin/com/bockerl/snailchat/chat/command/domain/service/CommandChatMessageServiceImpl.kt
@@ -5,8 +5,10 @@ import com.bockerl.snailchat.chat.command.application.dto.request.CommandChatMes
 import com.bockerl.snailchat.chat.command.application.service.CommandChatMessageService
 import com.bockerl.snailchat.chat.command.domain.aggregate.entity.ChatMessage
 import com.bockerl.snailchat.chat.command.domain.aggregate.enums.ChatMessageType
+import com.bockerl.snailchat.chat.command.domain.aggregate.enums.ChatRoomType
 import com.bockerl.snailchat.chat.command.domain.aggregate.event.CommandSendMessageEvent
 import com.bockerl.snailchat.chat.command.domain.repository.CommandChatMessageRepository
+import com.bockerl.snailchat.chat.query.service.QueryChatRoomService
 import com.bockerl.snailchat.infrastructure.outbox.dto.OutboxDTO
 import com.bockerl.snailchat.infrastructure.outbox.enums.EventType
 import com.bockerl.snailchat.infrastructure.outbox.service.OutboxService
@@ -15,21 +17,27 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.bson.types.ObjectId
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.time.Duration
 
 @Service
 class CommandChatMessageServiceImpl(
     private val simpleMessagingTemplate: SimpMessagingTemplate,
-    private val chatMessageRepository: CommandChatMessageRepository,
+    private val commandChatMessageRepository: CommandChatMessageRepository,
+    private val queryChatRoomService: QueryChatRoomService,
     private val kafkaMessageProducer: KafkaMessageProducer,
     private val objectMapper: ObjectMapper,
     private val outboxService: OutboxService,
+    private val redisTemplate: RedisTemplate<String, String>,
     @Value("\${spring.kafka.topic.personal-chat}")
     private val personalChatTopic: String,
     @Value("\${spring.kafka.topic.group-chat}")
     private val groupChatTopic: String,
+    @Value("\${spring.data.redis.idempotency-ttl-seconds}")
+    private val ttlSeconds: Long,
 ) : CommandChatMessageService {
     private val logger = KotlinLogging.logger { }
 
@@ -48,7 +56,7 @@ class CommandChatMessageServiceImpl(
             )
 
         // 전송할 메시지 DB에 저장
-        chatMessageRepository.save(chatMessage)
+        commandChatMessageRepository.save(chatMessage)
 
         // 해당 경로를 구독하고 있는 Client들에게 Message 전송
         simpleMessagingTemplate.convertAndSend("/topic/message/${chatMessage.chatRoomId}", chatMessage)
@@ -71,7 +79,7 @@ class CommandChatMessageServiceImpl(
             )
 
         // 전송할 메시지 DB에 저장
-        chatMessageRepository.save(chatMessage)
+        commandChatMessageRepository.save(chatMessage)
         logger.info { "메시지 DB 저장=$chatMessage" }
 
         // Kafka Producer를 통해 personalChatTopic 토픽에 메시지 전송
@@ -88,53 +96,80 @@ class CommandChatMessageServiceImpl(
 
     @Transactional
     override fun sendMessageByKafkaOutbox(updateMessageDTO: CommandChatMessageKeyRequestDTO) {
-        // 전송할 메시지 옮기기
-        val chatMessage =
-            ChatMessage(
-                chatRoomId = ObjectId(updateMessageDTO.chatRoomId),
-                memberId = updateMessageDTO.memberId,
-                memberNickname = updateMessageDTO.memberNickname,
-                memberPhoto = updateMessageDTO.memberPhoto,
-                message = updateMessageDTO.message,
-                messageType = updateMessageDTO.messageType,
-            )
-
-        // 전송할 메시지 DB에 저장
-        val savedChatMessage = chatMessageRepository.save(chatMessage)
-        logger.info { "메시지 DB 저장=$chatMessage" }
-
         // 멱등성 체크
-        if (outboxService.existsByIdempotencyKey(updateMessageDTO.idempotencyKey)) {
+        val checkIdempotencyKey =
+            redisTemplate
+                .opsForValue()
+                .setIfAbsent(updateMessageDTO.idempotencyKey, "1", Duration.ofSeconds(ttlSeconds))
+                ?: false
+
+        if (!checkIdempotencyKey) {
             logger.info { "중복 요청: 이미 처리된 idempotencyKey = ${updateMessageDTO.idempotencyKey}" }
             return
         }
 
-        // 도메인 이벤트 생성 (구독자에게 전달할 필요한 모든 정보 포함)
-        val sendMessageEvent =
-            CommandSendMessageEvent(
-                chatMessageId = savedChatMessage.id.toHexString(),
-                chatRoomId = savedChatMessage.chatRoomId.toHexString(),
-                memberId = savedChatMessage.memberId,
-                memberNickname = savedChatMessage.memberNickname,
-                memberPhoto = savedChatMessage.memberPhoto,
-                message = savedChatMessage.message,
-                messageType = savedChatMessage.messageType,
-            )
+        try {
+            val chatRoomId = ObjectId(updateMessageDTO.chatRoomId)
+            val memberId = updateMessageDTO.memberId
 
-        // 이벤트를 JSON 문자열로 직렬화
-        val jsonPayload = objectMapper.writeValueAsString(sendMessageEvent)
+            val (participantsCount, eventType) =
+                when (updateMessageDTO.chatRoomType) {
+                    ChatRoomType.PERSONAL -> {
+                        2 to EventType.PERSONAL_MESSAGE_SENT // 개인 채팅방은 참여자 항상 2명
+                    }
 
-        // Outbox 엔티티 생성 및 idempotencyKey를 통한 중복 방지
-        val outboxDTO =
-            OutboxDTO(
-                aggregateId = savedChatMessage.id.toHexString(), // objectId -> String
-                eventType = EventType.PERSONAL_MESSAGE_SENT, // 예시: 메시지 전송 이벤트 타입
-                payload = jsonPayload,
-                idempotencyKey = updateMessageDTO.idempotencyKey, // 클라이언트에서 제공한 고유 키
-            )
+                    ChatRoomType.GROUP -> {
+                        val participantsNum = queryChatRoomService.getGroupChatRoomParticipantsNum(chatRoomId)
+                        participantsNum to EventType.GROUP_MESSAGE_SENT
+                    }
+                }
 
-        // Outbox 등록
-        outboxService.createOutbox(outboxDTO)
+            val chatMessage =
+                ChatMessage(
+                    chatRoomId = chatRoomId,
+                    memberId = memberId,
+                    memberNickname = updateMessageDTO.memberNickname,
+                    memberPhoto = updateMessageDTO.memberPhoto,
+                    message = updateMessageDTO.message,
+                    messageType = updateMessageDTO.messageType,
+                    unreadCount = participantsCount - 1,
+                )
+
+            // 전송할 메시지 DB에 저장
+            val savedChatMessage = commandChatMessageRepository.save(chatMessage)
+            logger.info { "메시지 DB 저장=$chatMessage" }
+
+            // 도메인 이벤트 생성 (구독자에게 전달할 필요한 모든 정보 포함)
+            val sendMessageEvent =
+                CommandSendMessageEvent(
+                    chatMessageId = savedChatMessage.id.toHexString(),
+                    chatRoomId = savedChatMessage.chatRoomId.toHexString(),
+                    memberId = savedChatMessage.memberId,
+                    memberNickname = savedChatMessage.memberNickname,
+                    memberPhoto = savedChatMessage.memberPhoto,
+                    message = savedChatMessage.message,
+                    messageType = savedChatMessage.messageType,
+                )
+
+            // 이벤트를 JSON 문자열로 직렬화
+            val jsonPayload = objectMapper.writeValueAsString(sendMessageEvent)
+
+            // Outbox 엔티티 생성 및 idempotencyKey를 통한 중복 방지
+            val outboxDTO =
+                OutboxDTO(
+                    aggregateId = savedChatMessage.id.toHexString(), // objectId -> String
+                    eventType = eventType, // 예시: 메시지 전송 이벤트 타입
+                    payload = jsonPayload,
+                    idempotencyKey = updateMessageDTO.idempotencyKey, // 클라이언트에서 제공한 고유 키
+                )
+
+            // Outbox 등록
+            outboxService.createOutbox(outboxDTO)
+        } catch (ex: Exception) {
+            // 실패 시 Redis 키 삭제 → 클라이언트 재시도 시 다시 멱등성 검사 가능
+            redisTemplate.delete(updateMessageDTO.idempotencyKey)
+            throw ex // 트랜잭션 롤백
+        }
     }
 
     // Consumer가 Kafka Broker에서 받아온 메시지를 Stomp를 통해서 내부에 전달
@@ -162,7 +197,7 @@ class CommandChatMessageServiceImpl(
                 messageType = ChatMessageType.LEAVE,
             )
 
-        chatMessageRepository.save(chatMessage)
+        commandChatMessageRepository.save(chatMessage)
     }
 
     @Transactional
@@ -182,6 +217,6 @@ class CommandChatMessageServiceImpl(
                 messageType = ChatMessageType.ENTER,
             )
 
-        chatMessageRepository.save(enterMessage)
+        commandChatMessageRepository.save(enterMessage)
     }
 }
--- snail-chat/src/main/kotlin/com/bockerl/snailchat/infrastructure/config/RedisConfig.kt
+++ snail-chat/src/main/kotlin/com/bockerl/snailchat/infrastructure/config/RedisConfig.kt
@@ -1,10 +1,27 @@
 package com.bockerl.snailchat.infrastructure.config
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisSentinelConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.StringRedisSerializer
+import java.time.Duration
 
 @EnableCaching
 @Configuration
@@ -17,4 +34,80 @@ class RedisConfig(
     private val objectMapper: ObjectMapper,
 ) {
     private val logger = KotlinLogging.logger { }
+
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        val sentinelConfig =
+            RedisSentinelConfiguration().apply {
+                master(redisMaster)
+                logger.info { "redisMaster: " + redisMaster }
+                // localhost:port번호를 분리해서 대입
+                val (host1, port1) = parseHostPort(redisPort1)
+                sentinel(host1, port1)
+                val (host2, port2) = parseHostPort(redisPort2)
+                sentinel(host2, port2)
+                val (host3, port3) = parseHostPort(redisPort3)
+                sentinel(host3, port3)
+                setPassword(redisPassword)
+            }
+        return LettuceConnectionFactory(sentinelConfig)
+    }
+
+    // host:port 분리 ( redisPort1은 Localhost:포트번호 )
+    private fun parseHostPort(address: String): Pair<String, Int> {
+        val parts = address.split(":")
+        require(parts.size == 2) { "sentinel 주소가 잘못되었습니다: $address" }
+        return parts[0] to parts[1].toInt()
+    }
+
+    @Bean
+    fun redisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<String, Any> =
+        RedisTemplate<String, Any>().apply {
+            this.connectionFactory = redisConnectionFactory
+            this.keySerializer = StringRedisSerializer()
+
+            // ObjectMapper 설정
+            val objectMapper =
+                ObjectMapper().apply {
+                    // Kotlin 클래스 처리 설정
+                    registerModule(KotlinModule.Builder().build())
+                    // 타입 정보 포함
+                    activateDefaultTyping(
+                        LaissezFaireSubTypeValidator.instance,
+                        ObjectMapper.DefaultTyping.NON_FINAL,
+                        JsonTypeInfo.As.WRAPPER_ARRAY,
+                    )
+                    // Java 8 날짜/시간 모듈
+                    registerModule(JavaTimeModule())
+                    // 날짜/시간을 ISO-8601 형식 문자열로 직렬화
+                    disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                    // null 필드 처리
+                    setSerializationInclusion(JsonInclude.Include.NON_NULL)
+                    // 알 수 없는 프로퍼티 무시
+                    configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                }
+            // GenericJackson2JsonRedisSerializer 사용
+            val jsonRedisSerializer = GenericJackson2JsonRedisSerializer(objectMapper)
+            // 값 직렬화 설정
+            this.valueSerializer = jsonRedisSerializer
+            // Hash 작업을 위한 직렬화 설정
+            this.hashKeySerializer = StringRedisSerializer()
+            this.hashValueSerializer = jsonRedisSerializer
+
+            // 직렬화 설정 적용 된 후, 템플릿 초기화 : IllegalStateException 방지
+            afterPropertiesSet()
+        }
+
+    // 캐시 매니져
+    @Bean
+    fun cacheManager(redisConnectionFactory: RedisConnectionFactory): RedisCacheManager {
+        val configuration =
+            RedisCacheConfiguration
+                .defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(5)) // 캐시 TTL 5분 설정
+        return RedisCacheManager
+            .builder(redisConnectionFactory)
+            .cacheDefaults(configuration)
+            .build()
+    }
 }
